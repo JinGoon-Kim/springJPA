777c1c12528cdae7550955e9e20f14f749a54898
JinGoon-Kim <raneata@gmail.com>
2021-11-05 21:48:06 +0900

    added gavlyu

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -15,7 +14,6 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
@@ -27,5 +25,8 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        System.out.println("findMember == member: " + (findMember == member));
     }
 }
